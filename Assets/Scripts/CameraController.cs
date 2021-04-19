76fb04b7bfecda4242328abd548e457c7dcccd1e
Aihasas Yadu <yadu.aihasas@gmail.com>
2021-04-20 05:24:09 +0530

    Prototyping of the game Base structure of the game Basic Character Movement Scripts, Health Script, Camera Control Script

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private float xMargin = 1;
+    [SerializeField] private float yMargin = 1;
+    [SerializeField] private float xSmooth = 10;
+    [SerializeField] private float ySmooth = 10;
+    [SerializeField] private Vector2 maxXAndY;
+    [SerializeField] private Vector2 minXAndY;
+    [SerializeField] private Transform camTarget;
+
+    private bool CheckXMargin()
+    {
+        return Mathf.Abs(transform.position.x - camTarget.position.x) > xMargin;
+    }
+    private bool CheckYMargin()
+    {
+        return Mathf.Abs(transform.position.y - camTarget.position.y) > yMargin;
+    }
+
+    private void FixedUpdate()
+    {
+        TrackPlayer();
+    }
+
+    private void TrackPlayer()
+    {
+        float targetX = transform.position.x;
+        float targetY = transform.position.y;
+
+        if(CheckXMargin())
+        {
+            targetX = Mathf.Lerp(transform.position.x, camTarget.position.x, xSmooth * Time.deltaTime);
+        }
+
+        if(CheckYMargin())
+        {
+            targetY = Mathf.Lerp(transform.position.y, camTarget.position.y, ySmooth * Time.deltaTime);
+        }
+
+        targetX = Mathf.Clamp(targetX, minXAndY.x, maxXAndY.x);
+        targetY = Mathf.Clamp(targetY, minXAndY.y, maxXAndY.y);
+
+        transform.position = new Vector3(targetX, targetY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/SwordAttackController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordAttackController : MonoBehaviour
+{
+    private int lifeSpan = 2;
+    private void Start()
+    {
+        Destroy(gameObject, lifeSpan);
+    }
+}
