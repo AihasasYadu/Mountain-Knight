f60ed8b7b8ec16b3eed03f5a02b53124c178cafc
Aihasas Yadu <yadu.aihasas@gmail.com>
2021-04-21 02:52:01 +0530

    Prototyping Boss Mechanism Added BossController, BossHealth, Hand Triggers, Event Handler scripts Made Base structure for Boss

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+    private const string BOSS_AWAKE_PARAM = "BossAwake";
+    private bool isBossAwake = false;
+    private bool isBattling = false;
+    private bool isAttacking = false;
+
+    private float idleTimer = 0;
+    private float idleWaitTime = 10;
+    private float attackTimer = 0;
+    private float attackWaitTime = 2;
+    private int attackCount = 1;
+    [SerializeField] private Animator anim;
+    [SerializeField] private BossHealthController bossHealth;
+    [SerializeField] private BoxCollider handTrigger_L;
+    [SerializeField] private BoxCollider handTrigger_R;
+
+    public bool SetBattling { set { isBattling = value; } }
+    public bool IsBossAwake { get { return isBossAwake; } }
+    public bool SetBossAwake { set { isBossAwake = value; } }
+
+    private void Update()
+    {
+        WakeUp();
+    }
+
+    private void WakeUp()
+    {
+        if(isBossAwake)
+        {
+            anim.SetBool(BOSS_AWAKE_PARAM, true);
+            if(isBattling)
+            {
+                if(!isAttacking)
+                {
+                    idleTimer += Time.deltaTime;
+                }
+                else
+                {
+                    idleTimer = 0;
+                    attackTimer += Time.deltaTime;
+                    if(attackTimer >= attackWaitTime)
+                    {
+                        isAttacking = false;
+                        attackTimer = 0;
+                        handTrigger_L.enabled = true;
+                        handTrigger_R.enabled = true;
+                    }
+                }
+                if(idleTimer >= idleWaitTime)
+                {
+                    isAttacking = true;
+                    idleTimer = 0;
+                }
+            }
+            else
+            {
+                idleTimer = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/BossHealthController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossHealthController : MonoBehaviour
+{
+    [SerializeField] private int health = 100;
+    private BossController bossCtrlr;
+    public int DamageBoss { set { UpdateHealth(value); } }
+    public int GetBossHealth { get { return health; } }
+
+    private void Start()
+    {
+        bossCtrlr = GameManager.Instance.GetBossController;
+    }
+
+    private void BossDeath()
+    {
+        StartCoroutine(LevelRestart());
+    }
+
+    private void UpdateHealth(int damage)
+    {
+        health -= damage;
+        Debug.Log("Health" + health);
+        if(health <= 0)
+        {
+            health = 0;
+            BossDeath();
+        }
+    }
+
+    private IEnumerator LevelRestart()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
